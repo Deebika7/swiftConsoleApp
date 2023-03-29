30bee37597ffaea3151aca558c9ed81025dc5deb
Deebika G <deebikagovind@gmail.com>
2023-03-29 12:46:05 +0530

    Intial commit

---
+++ swiftConsoleApp/UserInterface/Menu/AdminMenu.swift
@@ -0,0 +1,8 @@
+//
+//  AdminMenu.swift
+//  swiftConsoleApp
+//
+//  Created by deebika-pt6680 on 29/03/23.
+//
+
+import Foundation
---
+++ swiftConsoleApp/UserInterface/Menu/CustomerMenu.swift
@@ -0,0 +1,8 @@
+//
+//  CustomerMenu.swift
+//  swiftConsoleApp
+//
+//  Created by deebika-pt6680 on 29/03/23.
+//
+
+import Foundation
